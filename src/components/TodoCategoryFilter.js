a216aede8d002ffb9bb7f4b5ec16981a6b6a88c1
Brandon Temba <brandontemba560@gmail.com>
2022-09-12 23:28:00 +0300

    Category file adde

--- src/components/TodoCategoryFilter.js
+++ src/components/TodoCategoryFilter.js
@@ -2,11 +2,10 @@ import React from "react";
 
 
 function TodoCategoryFilter({ todoCategories, selectedCategory, onCategorySelected }) {
-   
-   
+
     const categoryButtons = todoCategories.map((category) => {
         const className = (category === selectedCategory ? "selected" : null)
-       
+
         return (<button
             key={category}
             onClick={() => (onCategorySelected(category))}
@@ -15,7 +14,7 @@ function TodoCategoryFilter({ todoCategories, selectedCategory, onCategorySelect
           {category}
           </button>)
     })
-    return <div> {categoryButtons} </div>   
+    return <div> {categoryButtons} </div>
 }
 
 export default TodoCategoryFilter;
